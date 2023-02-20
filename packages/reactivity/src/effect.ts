322b8e26db6522a9d398984bfe7b8415ee02267e
heh <3209643266@qq.com>
2023-02-20 23:09:54 +0800

    fix：修复了在新增、删除以及迭代reactive对象时不触发依赖的问题；

--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -1,7 +1,7 @@
 import { queueJob } from '@vue/shared';
 import { reactive } from '.';
 import type { ObjectOfStringKey } from '../../shared/src/types';
-import type { Effect, Bucket, EffectOptions } from './types';
+import { Effect, Bucket, EffectOptions, optionType } from './types';
 
 let activeEffect: Effect | undefined;
 const activeEffectQueue: Effect[] = [];
@@ -34,38 +34,71 @@ export function effect(cb: () => any | void, options?: EffectOptions) {
   return EffectFn();
 };
 
-
-
-export function createReactive<T extends ObjectOfStringKey>(raw: T, isShallow: boolean): T { 
+// 这个key用来收集迭代器产生的副作用
+const ITERATE_KEY = Symbol();
+export function createReactive<T extends ObjectOfStringKey>(raw: T, isShallow: boolean, isReadonly: boolean = false): T { 
   return new Proxy(raw, {
-    get(target, key) {
-      // 暂时不考虑属性为 symbol 的情况
-      if (typeof key === 'symbol') return;
+    get(target, key, receiver) {
       if (isShallow && key === 'raw') return target;
-
-      track(target, key);
-      const res = Reflect.get(target, key);
-      if (isShallow && typeof res === 'object') { 
+      // 只读属性不会被修改，不需要收集
+      !isReadonly && track(target, key);
+      // 当被代理对象中有一个访问器属性 get value() { return this.name }
+      //  此时的this指向原始对象，无法触发set拦截函数
+      // reflect接受第三个参数，作为访问器属性中的this
+      const res = Reflect.get(target, key, receiver);
+      if (!isShallow && typeof res === 'object') { 
         return reactive(res);
       }
       return res;
     },
 
-    set(target, key, value) {
-      // 暂时不考虑属性为 symbol 的情况
-      if (typeof key === 'symbol') return false;
+    set(target, key, value, receiver) {
+      if (isReadonly) { 
+        console.warn('只读属性不可修改！');
+        return true;
+      };
+
+      const type = Reflect.has(target, key) ? optionType.set : optionType.add;
       if (Reflect.get(target, key) === value) return true;
 
       // 这里需要先赋值，以便后续执行副作用函数时能获取到被正确更新的值
-      const res = Reflect.set(target, key, value);
-      trigger(target, key);
+      const res = Reflect.set(target, key, value, receiver);
+      trigger(target, key, type);
       return res;
+    },
+
+    deleteProperty(target, key) { 
+      if (isReadonly) { 
+        console.warn('只读属性不可修改！');
+        return true;
+      };
+
+      const has1 = Reflect.has(target, key);
+      const res = Reflect.deleteProperty(target, key);
+      if (res && has1) { 
+        trigger(target, key, optionType.del);
+      };
+      return res;
+    },
+
+    // in 操作符会触发 has 拦截函数
+    has(target, key) { 
+      if (typeof key !== 'symbol') { 
+        track(target, key);
+      };
+      return Reflect.has(target, key);
+    },
+
+    // forin等迭代器会触发ownKeys拦截函数
+    ownKeys(target) { 
+      track(target, ITERATE_KEY);
+      return Reflect.ownKeys(target);
     },
   });
 };
 
 const bucket: Bucket = new WeakMap();
-export function track(target: ObjectOfStringKey, key: string) { 
+export function track(target: ObjectOfStringKey, key: string | symbol) { 
   if (!activeEffect) return;
 
   let targetMap = bucket.get(target);
@@ -82,14 +115,19 @@ export function track(target: ObjectOfStringKey, key: string) {
   activeEffect.deps.push(effectSet);
 };
 
-export function trigger(target: ObjectOfStringKey, key: string) { 
+export function trigger(target: object, key: string | symbol, type: optionType = optionType.add) { 
   const targetMap = bucket.get(target);
   if (!targetMap) return;
 
   const newEffectSet = new Set<Effect>(targetMap.get(key));
+  // 因为属性的 添加 和 删除 都会影响迭代器的结果，所以此处加入迭代器收集的依赖
+  if (type === optionType.add || type === optionType.del) { 
+    targetMap.get(ITERATE_KEY)?.forEach(iterateEffect => newEffectSet.add(iterateEffect));
+  };
+
   newEffectSet.forEach(effectFn => { 
-    // 当在副作用函数中同时读取、修改自身，会引起无限递归
-    // 原因在于：触发 set 函数时会循环检查依赖项并执行依赖，而依赖中的调用又会触发 get 收集依赖，在
+    // 当在副作用函数中同时读取、修改自身时，会导致在get拦截函数还未执行完毕时再调用set函数，如此无限循环
+    // 这行代码会阻止这一过程
     if (activeEffect === effectFn) return;
 
     const executeEffect = () => {
--- packages/reactivity/src/types.ts
+++ packages/reactivity/src/types.ts
@@ -7,11 +7,17 @@ export interface Effect {
 };
 
 export type EffectSet = Set<Effect>;
-export type TargetMap = Map<string, EffectSet>;
+export type TargetMap = Map<string | symbol, EffectSet>;
 export type Bucket = WeakMap<object, TargetMap>;
 
 export type EffectOptions = {
   lazy?: boolean,
-  onTrigger?: (target: object, key: string) => void,
+  onTrigger?: (target: object, key: string | symbol) => void,
   scheduler?: (effectFn: Effect) => void,
-}
+};
+
+export enum optionType { 
+  add = 'add',
+  set = 'set',
+  del = 'del',
+};
