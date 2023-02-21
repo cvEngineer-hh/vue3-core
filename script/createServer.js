af261cd2f0c76d84d18341cf895d93f8b273e97d
heh <3209643266@qq.com>
2023-02-21 15:02:22 +0800

    fix: 运行时自动规避已占用的端口

--- script/createServer.js
+++ script/createServer.js
@@ -2,16 +2,33 @@ const express = require('express');
 const path = require('path');
 const { exec } = require('node:child_process');
 
-const PortNumber = '3000';
-const { address } = require('os').networkInterfaces().WLAN.find(item => item.family === 'IPv4');
-const Network = `http://${address}:${PortNumber}`;
-const Local = `http://localhost:${PortNumber}`;
-
 const app = express();
-module.exports.createScerver = function() {
+module.exports.scerver = function scerver(portNumber = 3000) {
+  portIsActive(portNumber)
+    .then(res => {
+      createScerver(portNumber);
+    })
+    .catch(err => {
+      scerver(Number(portNumber) + 1);
+    });
+};
+
+function portIsActive(portNumber) {
+  return new Promise((res, rej) => {
+    const server = app.listen(portNumber);
+    server.on('error', rej);
+    server.on('listening', () => (res(), server.close()));
+  })
+};
+
+function createScerver(portNumber) {
+  const { address } = require('os').networkInterfaces().WLAN.find(item => item.family === 'IPv4');
+  const Network = `http://${address}:${portNumber}`;
+  const Local = `http://localhost:${portNumber}`;
+
   const startTime = Date.now();
   app.use('/', express.static(path.join(__dirname, '../dev')));
-  app.listen(PortNumber);
+  app.listen(portNumber);
 
   console.log('\033[42;30m启用服务器，\033[0m 耗时：', Date.now() - startTime, 'ms\033[0m');
   console.log('- Local: ' + '\033[34;4m' + Local + '\033[0m');
